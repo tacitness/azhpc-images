feb851317ad434ab1fa5edea54b576f1fd8fb518
rocky Cloud User <rocky@Rocky-8.10-HPC-x86-CycleCloud-Build-v2>
2025-03-17 23:51:34 +0000

    updated install_prerequisites, removed extraneous pre-install.sh

--- rocky/rocky-8.x/rocky-8.10-hpc/install_prerequisites.sh
+++ rocky/rocky-8.x/rocky-8.10-hpc/install_prerequisites.sh
@@ -3,11 +3,7 @@ set -ex
 
 # jq is needed to parse the component versions from the requirements.json file
 yum install -y jq wget tmux vim 
-##if ! yum localinstall -y files/*.rpm; then echo "Kernel Excluded;"; cat /etc/{yum.conf,dnf/dnf.conf}; rpm -qa | grep -i ^kernel; echo "exclude=kernel*" | tee -a /etc/dnf/dnf.conf -a /etc/yum.conf; fi
-
-# Uncomment on first build:
-#dnf install kernel{,-core,-modules,-tools}-4.18.0-553.5.1.el8_10.x86_64
-# reboot
-#dnf erase -y kernel{,-core,-modules,-tools}-4.18.0-553.8.1.el8_10.x86_64
 
 dnf --disableexcludes=all install kernel-rpm-macros kernel-{debug-devel,devel,headers,modules-extra}-$(uname -r)
+# Uncomment on first build:
+# reboot
